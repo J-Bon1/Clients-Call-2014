07e8f1197b5c73a2a5dc0090ebc87805e4e6e5e9
teefax64 <steffen.nagel@ymail.com>
2014-09-03 14:21:25 +0200

    blub

--- Game/Assets/Game/Block.cs
+++ Game/Assets/Game/Block.cs
@@ -4,12 +4,21 @@ using System.Collections;
 public class Block : MonoBehaviour {
 
 
-	public Texture picture;
-	public uint color; 
+	public Texture2D picture;
+	public Color color; 
+	public GameObject cube;
+	public GameObject plane;
+	// change this later to picture
+	public Texture2D dummypicture;
 
 	// Use this for initialization
 	void Start () {
 
+	}
+	public void create(){
+		cube.renderer.material.color = color;
+		plane.renderer.material.mainTexture = dummypicture;
+		print (dummypicture.name);
 	}
 	
 	// Update is called once per frame
