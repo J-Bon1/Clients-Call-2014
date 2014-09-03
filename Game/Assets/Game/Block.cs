c0ba0ebb7677d41ede34991415d57c348985ce4c
teefax64 <steffen.nagel@ymail.com>
2014-09-03 11:05:47 +0200

    added Blocks

---
+++ Game/Assets/Game/Block.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+
+	public Texture picture;
+	public uint color; 
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Game/Assets/Game/MapCreator.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+	
+
+public class MapCreator : MonoBehaviour {
+
+	//dellet this later
+	private int dummyX = 5;
+	private int dummyY = 5;
+
+
+
+	private Block[,] GameField;
+	private uint[] possibleColors;
+	private Texture[] possibleTextures;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void loadBrickTyps(){
+
+	}
+
+	private void createGameField(){
+		GameField  = new Block[dummyX, dummyY];
+	}
+	private void fillGameField(){
+		
+	}
+}
